8e2cb432834cd182749024e3343fec7541974fee
Neeta <neeta@creatoactive.com>
2026-03-03 20:24:18 +0530

    Move Firebase config to environment variables

--- src/auth/firebaseConfig.js
+++ src/auth/firebaseConfig.js
@@ -3,9 +3,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBRDeS_yfvp7Ddw1yx25GlPrQGDTtV_hFc",
-  authDomain: "patsanstha-6fa80.firebaseapp.com",
-  projectId: "patsanstha-6fa80",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: "patsanstha-6fa80.appspot.com",
   messagingSenderId: "207139234509",
   appId: "1:207139234509:web:d736d69ec1235ed4b1ff84",
